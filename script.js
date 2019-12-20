c5568b958a6933eab6527d6b238d398fbc760816
Michał Balicki <stiekerosiem@gmail.com>
2019-12-20 18:31:10 +0100

    first version of result

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 let firstNumber = 0;
 let secondNumber = 0;
+let result = 0;
+let operator;
 
 const keys = document.querySelector('.calculator-keys');
 
 keys.addEventListener('click', (event) => {
     const { target } = event;
     if (target.classList.contains('operator')) {
-        document.getElementById("textarea").value += target.value;
+        // document.getElementById("textarea").value += target.value;
+        firstNumber = document.getElementById("textarea").value;
+        document.getElementById("textarea").value = "";
+        operator = target.value;
+        console.log(operator);
     }
     else if (target.classList.contains('number')) {
         document.getElementById("textarea").value += target.value;
+    }
+    else if (target.classList.contains('equal-sign')) {
+        secondNumber = document.getElementById("textarea").value;
+        // console.log(firstNumber);
+        result = Number(firstNumber) + Number(secondNumber);
+        console.log("TCL: result", result)
+        String(result);
+        document.getElementById("textarea").value = result;
     }
 })

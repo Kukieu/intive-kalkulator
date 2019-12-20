618fa19c11022e2c979de70fcadeb7b47951f0c6
Michał Balicki <stiekerosiem@gmail.com>
2019-12-20 17:59:18 +0100

    number event listener added

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
+let firstNumber = 0;
+let secondNumber = 0;
+
 const keys = document.querySelector('.calculator-keys');
 
 keys.addEventListener('click', (event) => {
     const { target } = event;
     if (target.classList.contains('operator')) {
-        document.getElementById("textarea").value = target.value;
+        document.getElementById("textarea").value += target.value;
+    }
+    else if (target.classList.contains('number')) {
+        document.getElementById("textarea").value += target.value;
     }
 })

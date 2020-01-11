92c77d6958ce411712ebdb33b76763a48aae319c
Michał Balicki <stiekerosiem@gmail.com>
2020-01-11 19:41:46 +0100

    Update automaticTests.js

--- js/automaticTests.js
+++ js/automaticTests.js
@@ -1,136 +1,136 @@
-// for (let i = 0; i < 10; i++) {
-//     document.getElementById(i).click();
-//     if (document.getElementById('textarea').value === String(i)) {
-//         console.log('✔ button ' + i + " test passed");
-//     } else {
-//         console.log('𝐗 button ' + i + " test failed");
-//     }
-//     textareaReset();
-// }
+for (let i = 0; i < 10; i++) {
+    document.getElementById(i).click();
+    if (document.getElementById('textarea').value === String(i)) {
+        console.log('✔ button ' + i + " test passed");
+    } else {
+        console.log('𝐗 button ' + i + " test failed");
+    }
+    textareaReset();
+}
 
-// textareaReset();
+textareaReset();
 
-// document.getElementById(6).click();
-// document.getElementById("*").click();
-// document.getElementById(8).click();
-// document.getElementById("=").click();
-// if (document.getElementById('textarea').value === '48') {
-//     console.log("✔ 'multiply' test passed");
-// } else {
-//     console.log("𝐗 'multiply' test failed");
-// }
+document.getElementById(6).click();
+document.getElementById("*").click();
+document.getElementById(8).click();
+document.getElementById("=").click();
+if (document.getElementById('textarea').value === '48') {
+    console.log("✔ 'multiply' test passed");
+} else {
+    console.log("𝐗 'multiply' test failed");
+}
 
-// textareaReset();
+textareaReset();
 
-// document.getElementById(8).click();
-// document.getElementById("/").click();
-// document.getElementById(2).click();
-// document.getElementById("=").click();
-// if (document.getElementById('textarea').value === '4') {
-//     console.log("✔ 'divide' test passed");
-// } else {
-//     console.log("𝐗 'divide' test failed");
-// }
+document.getElementById(8).click();
+document.getElementById("/").click();
+document.getElementById(2).click();
+document.getElementById("=").click();
+if (document.getElementById('textarea').value === '4') {
+    console.log("✔ 'divide' test passed");
+} else {
+    console.log("𝐗 'divide' test failed");
+}
 
-// textareaReset();
+textareaReset();
 
-// document.getElementById(8).click();
-// document.getElementById("+").click();
-// document.getElementById(2).click();
-// document.getElementById("=").click();
-// if (document.getElementById('textarea').value === '10') {
-//     console.log("✔ '+' test passed");
-// } else {
-//     console.log("𝐗 '+' test failed");
-// }
+document.getElementById(8).click();
+document.getElementById("+").click();
+document.getElementById(2).click();
+document.getElementById("=").click();
+if (document.getElementById('textarea').value === '10') {
+    console.log("✔ '+' test passed");
+} else {
+    console.log("𝐗 '+' test failed");
+}
 
-// textareaReset();
+textareaReset();
 
-// document.getElementById(8).click();
-// document.getElementById("-").click();
-// document.getElementById(2).click();
-// document.getElementById("=").click();
-// if (document.getElementById('textarea').value === '6') {
-//     console.log("✔ '-' test passed");
-// } else {
-//     console.log("𝐗 '-' test failed");
-// }
+document.getElementById(8).click();
+document.getElementById("-").click();
+document.getElementById(2).click();
+document.getElementById("=").click();
+if (document.getElementById('textarea').value === '6') {
+    console.log("✔ '-' test passed");
+} else {
+    console.log("𝐗 '-' test failed");
+}
 
-// textareaReset();
+textareaReset();
 
-// document.getElementById(8).click();
-// document.getElementById(".").click();
-// document.getElementById(2).click();
-// document.getElementById("+").click();
-// document.getElementById(0).click();
-// document.getElementById(".").click();
-// document.getElementById(8).click();
-// document.getElementById("=").click();
-// if (document.getElementById('textarea').value === '9') {
-//     console.log("✔ 'float' test passed");
-// } else {
-//     console.log("𝐗 'float' test failed");
-// }
+document.getElementById(8).click();
+document.getElementById(".").click();
+document.getElementById(2).click();
+document.getElementById("+").click();
+document.getElementById(0).click();
+document.getElementById(".").click();
+document.getElementById(8).click();
+document.getElementById("=").click();
+if (document.getElementById('textarea').value === '9') {
+    console.log("✔ 'float' test passed");
+} else {
+    console.log("𝐗 'float' test failed");
+}
 
-// textareaReset();
+textareaReset();
 
-// document.getElementById(8).click();
-// document.getElementById(2).click();
-// document.getElementById("clear-all").click();
-// if (document.getElementById('textarea').value === '') {
-//     console.log("✔ 'clear-all' test passed");
-// } else {
-//     console.log("𝐗 'clear-all' test failed");
-// }
+document.getElementById(8).click();
+document.getElementById(2).click();
+document.getElementById("clear-all").click();
+if (document.getElementById('textarea').value === '') {
+    console.log("✔ 'clear-all' test passed");
+} else {
+    console.log("𝐗 'clear-all' test failed");
+}
 
-// textareaReset();
+textareaReset();
 
-// document.getElementById(8).click();
-// document.getElementById(2).click();
-// document.getElementById("clear-last").click();
-// if (document.getElementById('textarea').value === '8') {
-//     console.log("✔ 'clear-last' test passed");
-// } else {
-//     console.log("𝐗 'clear-last' test failed");
-// }
+document.getElementById(8).click();
+document.getElementById(2).click();
+document.getElementById("clear-last").click();
+if (document.getElementById('textarea').value === '8') {
+    console.log("✔ 'clear-last' test passed");
+} else {
+    console.log("𝐗 'clear-last' test failed");
+}
 
-// textareaReset();
+textareaReset();
 
-// document.getElementById(2).click();
-// document.getElementById("+").click();
-// document.getElementById(2).click();
-// document.getElementById("*").click();
-// document.getElementById(2).click();
-// document.getElementById("=").click();
-// if (document.getElementById('textarea').value === '6') {
-//     console.log("✔ 'Sequence of Operations 1' test passed");
-// } else {
-//     console.log("𝐗 'Sequence of Operations 1' test failed");
-// }
+document.getElementById(2).click();
+document.getElementById("+").click();
+document.getElementById(2).click();
+document.getElementById("*").click();
+document.getElementById(2).click();
+document.getElementById("=").click();
+if (document.getElementById('textarea').value === '6') {
+    console.log("✔ 'Sequence of Operations 1' test passed");
+} else {
+    console.log("𝐗 'Sequence of Operations 1' test failed");
+}
 
-// textareaReset();
+textareaReset();
 
-// document.getElementById(1).click();
-// document.getElementById("+").click();
-// document.getElementById(2).click();
-// document.getElementById("*").click();
-// document.getElementById('(').click();
-// document.getElementById('3').click();
-// document.getElementById('+').click();
-// document.getElementById('4').click();
-// document.getElementById(')').click();
-// document.getElementById('/').click();
-// document.getElementById(2).click();
-// document.getElementById('-').click();
-// document.getElementById(1).click();
-// document.getElementById("=").click();
-// if (document.getElementById('textarea').value === '7') {
-//     console.log("✔ 'Sequence of Operations 2' test passed");
-// } else {
-//     console.log("𝐗 'Sequence of Operations 2' test failed");
-// }
+document.getElementById(1).click();
+document.getElementById("+").click();
+document.getElementById(2).click();
+document.getElementById("*").click();
+document.getElementById('(').click();
+document.getElementById('3').click();
+document.getElementById('+').click();
+document.getElementById('4').click();
+document.getElementById(')').click();
+document.getElementById('/').click();
+document.getElementById(2).click();
+document.getElementById('-').click();
+document.getElementById(1).click();
+document.getElementById("=").click();
+if (document.getElementById('textarea').value === '7') {
+    console.log("✔ 'Sequence of Operations 2' test passed");
+} else {
+    console.log("𝐗 'Sequence of Operations 2' test failed");
+}
 
-// textareaReset();
+textareaReset();
 
 document.getElementById(1).click();
 document.getElementById("+").click();
@@ -188,8 +188,10 @@ document.getElementById(')').click();
 document.getElementById(')').click();
 document.getElementById('-').click();
 document.getElementById('2').click();
+document.getElementById('.').click();
+document.getElementById('5').click();
 document.getElementById("=").click();
-if (document.getElementById('textarea').value === '2') {
+if (document.getElementById('textarea').value === '1.5') {
     console.log("✔ 'Sequence of Operations with power and roots' test passed");
 } else {
     console.log("𝐗 'Sequence of Operations with power and roots' test failed");
@@ -197,11 +199,6 @@ if (document.getElementById('textarea').value === '2') {
 
 textareaReset();
 
-// document.getElementById(3).click();
-// document.getElementById("power").click();
-// document.getElementById(2).click();
-// document.getElementById('=').click();
-
 function textareaReset() {
-    document.getElementById('textarea').value = '';
+    document.getElementById("clear-all").click();
 }
